bb837a1b7502955f4500ef7bcb2936ca7334742d
Unknown <lupescua@gmail.com>
2018-01-30 16:14:11 +0100

    Moved JS from HTML to JS file

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
+$('.beard').appear();
+console.log('Ola');
 
-$('.progress').appear();
 $('.progress').on('appear', function () {
-    alert('Hy');
-    progress('#HTML', '40%');
-    progress('#CSS/SCSS/SASS', '35%');
-    progress('#JavaScript', '25%');
-    progress('#jQuery ', '30%');
-    progress('#Python ', '15%');
-    progress('#VBA', '30%');
+console.log('Hy');
+progress('#HTML', '40%');
+progress('#CSS', '35%');
+progress('#JavaScript', '25%');
+progress('#jQuery ', '30%');
+progress('#Python ', '15%');
+progress('#VBA', '30%');
 });
 
 function progress(barID, value) {
-    $(barID).animate({
-        'width': value
-    }, 2000);
+$(barID).animate({
+    'width': value
+}, 2000);
 };
