0b166c6b8b439165be1afd9593257fa1fe990aa8
Unknown <lupescua@gmail.com>
2018-01-31 09:22:44 +0100

    Made the Portfolio Bar be animated

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,16 @@
-$('.beard').appear();
-console.log('Ola');
-
+$('.progress').appear();
 $('.progress').on('appear', function () {
-console.log('Hy');
-progress('#HTML', '40%');
-progress('#CSS', '35%');
-progress('#JavaScript', '25%');
-progress('#jQuery ', '30%');
-progress('#Python ', '15%');
-progress('#VBA', '30%');
+    console.log('Hy');
+    progress('#HTML', '40%');
+    progress('#CSS', '35%');
+    progress('#JavaScript', '25%');
+    progress('#jQuery ', '30%');
+    progress('#Python ', '15%');
+    progress('#VBA', '30%');
 });
 
 function progress(barID, value) {
-$(barID).animate({
-    'width': value
-}, 2000);
+    $(barID).animate({
+        'width': value
+    }, 2000);
 };
